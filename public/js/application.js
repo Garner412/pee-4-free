2d268d2d0f518c8f8c6f82b174a03a7f1bb3d6db
Greg Garner <email": null>
2016-09-29 18:01:34 -0500

    trying to pee

--- public/js/application.js
+++ public/js/application.js
@@ -17,11 +17,27 @@ function placeMarker(latitude, longitude, map) {
     position: {lat: latitude, lng: longitude},
     map: map
   });
-  $.when(findAddress(longitude, latitude, marker)).then(function(){
-    marker.info = new google.maps.InfoWindow({content: marker.address + '<br>' + marker.name})
-    marker.addListener('click', showInfoWindow)
-  })
+    marker.info = new google.maps.InfoWindow({content: "<table>" + "<input type='hidden' name=id, value="+marker.id+">"+
+                 "<tr><td>Name:</td> <td><input type='text' id='name'/> </td> </tr>" +
+                 "<tr><td>Ranking:</td> <td><select id='ranking'>" +
+                 "<option value=5 SELECTED>5</option>" +
+                 "<option value=4>4</option>" +
+                 "<option value=3>3</option>" +
+                 "<option value=2>2</option>" +
+                 "<option value=1>1</option>" +
+                 "</select> </td></tr>" +
+                 "<tr><td></td><td><input type='button' value='Save & Close' onclick='saveData(marker)'/></td></tr>"})
 
+  $.when(findAddress(longitude, latitude, marker)).then(function(){
+    marker.addListener('click', showInfoWindow);
+  })
+    marker.info.open(map, marker);
+
+}
+function saveData(id){
+  console.log(id)
+  // marker.name = document.getElementById("name").value;
+  // marker.ranking = document.getElementById("ranking").value;
 }
 
 function findAddress(longitude, latitude, marker){
@@ -32,7 +48,7 @@ function findAddress(longitude, latitude, marker){
   .done(function(response) {
     var addressObject = JSON.parse(response);
     formattedAddress = addressObject.results[0].formatted_address;
-    createBathroom({latitude: latitude, longitude: longitude, address: formattedAddress});
+    createBathroom({latitude: latitude, longitude: longitude, address: formattedAddress, name: marker.name, ranking: marker.ranking});
     marker.address = formattedAddress
   });
   return request
