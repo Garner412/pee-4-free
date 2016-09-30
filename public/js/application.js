a51754db7b0d065ff2e4b4591f1a6a34882f8355
Dan Dobrick <dandobrick@gmail.com>
2016-09-29 22:04:43 -0500

    zoom in

--- public/js/application.js
+++ public/js/application.js
@@ -3,7 +3,7 @@ var markers = [];
 function initMap() {
  map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 41.881, lng: -87.623},
-    zoom: 5
+    zoom: 20
   });
 
  if (navigator.geolocation) {
