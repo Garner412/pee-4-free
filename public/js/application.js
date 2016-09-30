f7702ab596b6aae3c8ee74eae9d634890d8a5c09
Dan Dobrick <dandobrick@gmail.com>
2016-09-29 21:51:28 -0500

    Add Geolocation

--- public/js/application.js
+++ public/js/application.js
@@ -6,6 +6,21 @@ function initMap() {
     zoom: 10
   });
 
+ if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(function(position) {
+    var pos = {
+      lat: position.coords.latitude,
+      lng: position.coords.longitude
+    };
+
+    infoWindow.setPosition(pos);
+    infoWindow.setContent('Location found.');
+    map.setCenter(pos);
+  }, function() {
+    handleLocationError(true, infoWindow, map.getCenter());
+  });
+}
+
 }
 
 function showInfoWindow(){
